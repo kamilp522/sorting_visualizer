c7fbc68342f08061c4866cc3986ce154e7ea3b7e
Kamil Podgórski <kamilp522@gmail.com>
2022-12-31 15:06:23 +0100

    work on insertion sort in progress

--- src/components/algorithms/InsertionSort.jsx
+++ src/components/algorithms/InsertionSort.jsx
@@ -15,7 +15,27 @@ const InsertionSort = ({
     if (isSorting) return;
     setIsSorting(true);
 
+    for (let i = 1; i < blockNodesHeights.length - 1; i++) {
+      let blocks = [...document.querySelectorAll(".main__block")];
+
+      const previousBlockHeight = parseHeight(blocks[i - 1]);
+      const currentBlockHeight = parseHeight(blocks[i]);
+
+      for (let j = i; j > 0 && previousBlockHeight > currentBlockHeight; j--) {
+        swapBlocks(blocks[j], blocks[j - 1]);
+      }
+    }
+
     setIsSorting(false);
+  };
+
+  const swapBlocks = (current, previous) => {
+    const blockContainer = document.getElementById("block-container");
+    if (!blockContainer) return;
+
+    console.log("dziala");
+
+    blockContainer.insertBefore(current, previous);
   };
 
   return (
